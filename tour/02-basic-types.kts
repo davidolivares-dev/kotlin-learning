4a134e8230db3c95a7266fd1eb05d631dbb1fc3a
David Olivares <david.olivares.dev@gmail.com>
2026-06-08 21:35:59 -0700

    Add basic types tour chapter

---
+++ tour/02-basic-types.kts
@@ -0,0 +1,17 @@
+#!/usr/bin/env kotlin
+
+// Explicitly declare the correct type for each variable:
+// val a: Int = 1000
+// val b = "log message"
+// val c = 3.14
+// val d = 100_000_000_000_000
+// val e = false
+// val f = '\n'
+val a: Int = 1000
+val b: String = "log message"
+val c: Double = 3.14
+val d: Long = 100_000_000_000_000
+val e: Boolean = false
+val f: Char = '\n'
+
+println("$a | $b | $c | $d | $e | $f")
