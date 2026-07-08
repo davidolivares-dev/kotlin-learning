a418ff99d14e464e18f75992690030f1cb666142
David Olivares <david.olivares.dev@gmail.com>
2026-07-07 21:24:53 -0700

    Add lambdas koan

---
+++ koans/introduction/lambdas/Task.kt
@@ -0,0 +1,9 @@
+/*
+ * Kotlin Koans — Introduction #8: Lambdas
+ *
+ * Task: Pass a lambda to the any function to check whether the collection
+ * contains an even number.
+ */
+
+fun containsEven(collection: Collection<Int>): Boolean =
+    collection.any { i -> i % 2 == 0}
